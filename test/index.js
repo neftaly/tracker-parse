4ab335475cd1c4b17dde9846c6885a820d595a10
Neftaly Hernandez <neftaly.hernandez@gmail.com>
2017-05-10 22:36:18 +1200

    Auto-create test result dir

--- test/index.js
+++ test/index.js
@@ -1,8 +1,21 @@
 import tape from 'tape';
-import fs from 'fs';
-import { parser, states } from '../src';
+import {
+  readFileSync,
+  writeFileSync,
+  mkdirSync,
+  existsSync
+} from 'fs';
+import {
+  parser,
+  states
+} from '../src';
 
-const rawFile = fs.readFileSync(
+const resultsDir = './test/results';
+if (!existsSync(resultsDir)) {
+  mkdirSync(resultsDir);
+}
+
+const rawFile = readFileSync(
   './test/fixtures/tracker_2017_04_18_12_31_11.PRdemo'
 );
 
@@ -11,8 +24,8 @@ const parsed = parser(rawFile);
 const aEnd = new Date();
 console.warn('Parse time: ' + ((aEnd - aStart) / 1000) + ' sec');
 
-fs.writeFileSync(
-  './test/results/tracker_2017_04_18_12_31_11.json',
+writeFileSync(
+  resultsDir + '/tracker_2017_04_18_12_31_11.json',
   JSON.stringify(parsed, null, 2)
 );
 
@@ -21,8 +34,8 @@ const result = states(undefined, parsed);
 const bEnd = new Date();
 console.warn('State time: ' + ((bEnd - bStart) / 1000) + ' sec');
 
-fs.writeFileSync(
-  './test/results/tracker_2017_04_18_12_31_11.state.json',
+writeFileSync(
+  resultsDir + '/tracker_2017_04_18_12_31_11.state.json',
   JSON.stringify(result.last(), null, 2)
 );
 
